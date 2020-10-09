81bb16c9ef334754607fd40b7644525b28020bbe
Mike Fallesen <mfallesen2010@gmail.com>
2020-10-09 00:07:28 -0700

    remove console.logs and unused get

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -7,11 +7,6 @@ function apiRoutes(app) {
         res.json(db);
     })
 
-    app.get("/api/notes/:id", function(req, res) {
-
-        res.json(data[Number(req.params.id)]);
-        console.log(req.params.id);
-    });
 
     app.post("/api/notes", function(req, res) {
         const newNote = req.body;
@@ -27,12 +22,12 @@ function apiRoutes(app) {
 
     app.delete("/api/notes/:id", function(req, res){
         const removeNote = req.params.id;
-        console.log(req.params.id);
+        
         db.pop(removeNote);
-        console.log(removeNote);
+        
         fs.writeFile("../db/db.json", JSON.stringify(db), function(err) {
         })
-        res.send("DELETE request called")
+        
         
     })
 }
