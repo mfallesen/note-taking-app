d996712a3e0c43bf84156f5cbb0845016e07ad18
Mike Fallesen <mfallesen2010@gmail.com>
2020-10-08 21:21:05 -0700

    added Index.html route

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -1,11 +1,13 @@
 // Dependencies
 const path = require("path");
 
-
-
 function htmlRoutes(app) {
     app.get("/notes", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/notes.html"));
+    })
+
+    app.get("*", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"))
     })
 }
 module.exports = htmlRoutes;
